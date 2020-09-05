d906f0eef7021f8b4a692357bd850351bfb7fcb6
Vinícius Vargas Ribeiro <vinicius.vargas@conectt.com.br>
2020-09-05 15:44:21 -0300

    adicionado node schedule

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+var schedule = require('node-schedule');
 
 require("./models/Users");
 const Users = mongoose.model('users');
