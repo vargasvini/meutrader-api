edae997aa3dc28c841ad22d22bb1fbc2462e3e00
Vinícius Vargas Ribeiro <vinicius.vargas@conectt.com.br>
2020-09-07 18:05:19 -0300

    adicionado https forçado

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const path = require('path');
 var schedule = require('node-schedule');
 
-app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
-    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP 
-        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
-    else //Se a requisição já é HTTPS 
-        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
-});
+// app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
+//     if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP 
+//         res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
+//     else //Se a requisição já é HTTPS 
+//         next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
+// });
 
 require("./models/Users");
 const Users = mongoose.model('users');
